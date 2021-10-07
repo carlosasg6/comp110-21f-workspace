2931e1819059f19fdc89143e9383812e7324b346
Carlos Sosa <carlos6@live.unc.edu>
2021-10-07 03:10:32 -0400

    progress on ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,46 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730330989"
 
 # Define your functions below
+
+
+def only_evens(xs: list[int]) -> list[int]:
+    """This is a function that when given a list of ints will return the even ints."""
+    # run through the list
+    i: int = 0
+    ys: list[int] = list()
+    while i < len(xs):
+        if xs[i] % 2 == 0:
+            ys.append(xs[i])
+        i += 1
+    return ys
+
+
+def sub(xs: list[int], start: int, end: int) -> list[int]:
+    """A function that takes a list, a start index and a end index that returns a list that is a subset of the one given, between the specified start index and end index -1."""
+    ys: list[int] = list()
+    if len(xs) == 0:
+        return xs
+    if start < 0:
+        start = 0
+    if end > len(xs) - 1:
+        end = len(xs) - 1
+    while start < end:
+        ys.append(xs[start])
+        start += 1
+    return ys
+
+
+def concat(xs: list[int], ys: list[int]) -> list[int]:
+    """Function that inputs two lists, and outputs a list containing all the elements of the first list followd by the elements of the second list."""
+    i: int = 0
+    j: int = 0
+    zs: list[int] = list()
+    while i < len(xs):
+        zs.append(xs[i])
+        i += 1
+    while j < len(ys):
+        zs.append(ys[j])
+        j += 1
+    return zs
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,4 +3,60 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730330989"
+
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+def test_only_evens_none() -> None:
+    xs: list[int] = [1, 5, 3]
+    assert only_evens(xs) == []
+
+
+def test_only_evens_all() -> None:
+    xs: list[int] = [4, 2, 4, 4]
+    assert only_evens(xs) == [4, 2, 4, 4]
+
+
+def test_only_evens_some() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    assert only_evens(xs) == [2, 4]
+
+
+def test_sub_use() -> None:
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    start: int = 1
+    end: int = 5
+    assert sub(xs, start, end) == [2, 3, 4, 5]
+
+
+def test_sub_use_ex() -> None:
+    xs: list[int] = [10, 20, 30, 40]
+    start: int = 1
+    end: int = 3
+    assert sub(xs, start, end) == [20, 30]
+
+
+def test_sub_neg_start_long_end() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    start: int = -1
+    end: int = 6
+    assert sub(xs, start, end) == [1, 2, 3]
+
+
+def test_concat_nothin() -> None:
+    xs: list[int] = []
+    ys: list[int] = []
+    assert concat(xs, ys) == []
+
+
+def test_concat_lotta() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    ys: list[int] = [5, 6, 7, 8]
+    assert concat(xs, ys) == [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def test_concat_negs() -> None:
+    xs: list[int] = [-1, -2, -3]
+    ys: list[int] = [10, -30, 20]
+    assert concat(xs, ys) == [-1, -2, -3, 10, -30, 20]
